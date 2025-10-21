241f9690abed06952ab8583cd9a434513b3d5b7a
NemoDragon <adamtokarz.kol@gmail.com>
2025-10-21 12:03:06 +0200

    plotting results

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
+
 def read_file(file_name: str):
     best_fitness = []
     with open(file_name) as file:
@@ -10,15 +11,21 @@ def read_file(file_name: str):
                 best_fitness.append(float(line.split(" ")[5]))
     return best_fitness
 
-fitness = read_file("genetic_algorithm_log_mutation_lux_10.txt")
-print(fitness)
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-gen = [i for i in range(len(fitness))]
-ax.plot(gen, fitness)
-ax.set_xlabel("Generation")
-ax.set_ylabel("Fitness")
-# ax.set_yscale("log")
-ax.set_title("Fitness function")
-plt.show()
+def main() -> None:
+    for i in range(11, 31):
+        fitness = read_file(f'results/genetic_algorithm_{i}.txt')
+        print(fitness)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        gen = [i for i in range(len(fitness))]
+        ax.plot(gen, fitness)
+        ax.set_xlabel("Generation")
+        ax.set_ylabel("Fitness")
+        # ax.set_yscale("log")
+        ax.set_title(f"Fitness function: GA with 1 position, run {i - 10} " if i < 21 else f"Fitness function: GA with 3 positions, run {i % 20} ")
+        plt.show()
+
+if __name__ == "__main__":
+    main()
